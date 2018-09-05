97313e03b45f51b9a5567e5a0e61b2b13e45f195
38084 <38084@DESKTOP-2QG5MTD>
2018-09-05 16:08:41 +0800

    StuSrvImpl Update

--- src/VC/server/bz/StuSrvImpl.java
+++ src/VC/server/bz/StuSrvImpl.java
@@ -11,6 +11,8 @@ import java.util.List;
 import VC.common.Book;
 import VC.common.BookMessage;
 import VC.common.Message;
+import VC.common.User;
+import VC.common.UserMessage;
 import VC.server.dao.LibraryDAO;
 import VC.server.dao.StuDAO;
 import VC.server.vo.LibrarySrv;
@@ -30,11 +32,30 @@ public class StuSrvImpl implements StuSrv{
 		
 	}
 	
-	public void getInfo(Message rcvmsg, Socket socket) {
-		
+	public void getInfo(Message rcvmsg, Socket socket) throws SQLException, IOException {
+		User inf = new User();
+		UserMessage Usermsg = new UserMessage();
+		Usermsg = (UserMessage) rcvmsg;
+		UserMessage sendmsg = new UserMessage();
+
+		inf = studao.getUserInf(Usermsg.getUsername());
+		sendmsg.setUser(inf);;
+
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+		oos.writeObject(sendmsg);
+		oos.flush();
 	}
 	
-	public void updateInfo(Message rcvmsg, Socket socket) {
+	public void updateInfo(Message rcvmsg, Socket socket) throws SQLException, IOException {
+		UserMessage Usermsg = new UserMessage();
+		Usermsg = (UserMessage)rcvmsg;
+		UserMessage sendmsg = new UserMessage();
+		String username = Usermsg.getUsername();
 		
+		sendmsg.setRes(studao.modifyinf(username, Usermsg.getUser()));
+		
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+		oos.writeObject(sendmsg);
+		oos.flush();
 	}
 }
